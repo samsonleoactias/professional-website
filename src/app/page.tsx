9932dc95b91e5b7a0110ad9f7e268e7b9c43fd84
Samson Actias <hello@samsonleoactias.com>
2024-05-12 15:39:09 -0400

    expanded tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { Box, Tab, Tabs, Typography } from "@mui/material";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface TabPanelProps {
   index: number;
@@ -21,7 +22,7 @@ const CustomTabPanel = (props: TabPanelProps) => {
 };
 
 const Page = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(5);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -60,9 +61,288 @@ const Page = () => {
           />
         </Tabs>
       </Box>
-      <CustomTabPanel value={value} index={0}></CustomTabPanel>
-      <CustomTabPanel value={value} index={1}></CustomTabPanel>
-      <CustomTabPanel value={value} index={2}></CustomTabPanel>
+      <CustomTabPanel value={value} index={0}>
+        <Typography
+          sx={{
+            fontSize: "25px",
+          }}
+        >
+          Who I am:
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--a full stack software engineer
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--a problem solver
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--a constant learner and educator
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--a former apprentice electrician, bar
+          server, and New York Fashion Week photographer's assistant
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "25px",
+            borderTop: "2px solid #DD5746",
+            width: "45%",
+            marginTop: "30px",
+          }}
+        >
+          What I have:
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--4 years of experience in front end,
+          back end, API, and database engineering
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--devotion to the customer and user
+          experience, as evidenced by the Customer Shoes Award Honorable Mention
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I
+          was given by my former employer Bandwidth
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--first-hand experience with AI from my
+          role at Librestream where I designed a new knowledge management
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;product
+          featuring machine learning for finding information quickly and where I
+          built the first prototype of said project
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--a proven track record of coding
+          solutions to problems, including building an app at Bandwidth that
+          enabled
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;contracted
+          pen testers to increase their testing coverage of my team's
+          application from 50% to 100%
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "25px",
+            borderTop: "2px solid #DD5746",
+            width: "45%",
+            marginTop: "30px",
+          }}
+        >
+          What I want to bring to your team:
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--communication skills for discussing
+          solutions to problems and deciding on design approaches for new
+          software
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--comfort in working on-call shifts
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--technical skills and logical problem
+          solving ability, which I developed both in software work and in my
+          undergraduate education in mathematics
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--a willingness to receive feedback so I
+          can improve my work and skills
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "25px",
+            borderTop: "2px solid #DD5746",
+            width: "45%",
+            marginTop: "30px",
+          }}
+        >
+          How you can contact me:
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--hello at samsonleoactias dot com
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--linkedin.com/in/samsonleoactias
+        </Typography>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Typography
+          sx={{
+            fontSize: "25px",
+          }}
+        >
+          <Link
+            href="https://docs.google.com/document/d/1nt7lgNoiZu-PiniS0I7-T7_UmCF1voFc9NeQX8KEKM0/edit?usp=sharing"
+            target="_blank"
+          >
+            Link: Hosted on Google Docs
+          </Link>
+        </Typography>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <Typography
+          sx={{
+            fontSize: "25px",
+          }}
+        >
+          Chess:
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Built with a
+          React/NextJS front end, a NodeJS backend, and an GraphQL API.
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Currently the
+          computer player algorithm isn't very sophisticated.
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It mostly only
+          knows to prioritize capturing enemy pieces and how to get itself out
+          of check.
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Features on my
+          to do list:
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--auth
+          so players can keep track of wins, loses, and ties
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--a
+          much smarter computer player
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "25px",
+          }}
+        >
+          <Link
+            href="https://github.com/samsonleoactias/chess-game-server"
+            target="_blank"
+          >
+            Link: Github for server
+          </Link>
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "25px",
+          }}
+        >
+          <Link
+            href="https://github.com/samsonleoactias/chess-ui"
+            target="_blank"
+          >
+            Link: Github for UI
+          </Link>
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "25px",
+          }}
+        >
+          <Link
+            href="https://chess-ui-de2f40ad29e2.herokuapp.com/game"
+            target="_blank"
+          >
+            Link: Website
+          </Link>
+        </Typography>
+      </CustomTabPanel>
       <CustomTabPanel value={value} index={3}>
         <Typography
           sx={{
